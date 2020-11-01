0304d5cdac07a84ccdf9c5a0ae1a47af1a832332
pedro-dominici <pedro.dominici@acct.global>
2020-11-01 18:19:07 -0300

    home + header

---
+++ src/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto PHP</title>
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/header.css">
+</head>
+<header >
+    <div class="header-menu logo">
+        <h1>EletroFacil</h1>
+        <div class="header-login">
+            <a href="#" class="desktop">Carrinho</a>
+            <a href="#" class="desktop">Login</a>
+        </div>
+    </div>
+    <div class="header-menu menu">
+        <a href="#">arduino</a>
+        <a href="#">componentes</a>
+        <a href="#">promoção</a>
+        <a href="#" class="mobile">carrinho</a>
+        <a href="#" class="mobile">login</a>
+    </div>
+</header>
+</html>
---
+++ src/home.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="../css/home.css">
+</head>
+<body>
+    <?php
+        include('./header.php')
+    ?>
+    <div class="container-home">    
+        <div class="categorias">
+                    <a href="#">
+                        <img src="../img/arduino.png" alt="">
+                    </a>
+
+                    <a href="#">
+                        <img src="../img/componentes.png" alt="">
+                    </a>
+                    <a href="#">
+                        <img src="../img/placas.png" alt="">
+                    </a>
+                
+                    <a href="#">
+                        <img src="../img/solda.png" alt="">
+                    </a>
+
+                    <a href="#">
+                        <img src="../img/chips.png" alt="">
+                    </a>
+        </div>
+
+        
+        <div class="top-vendas">
+            <h1>TOP<span>VENDAS</span></h1>
+            <div class="product-list">
+                <div class="product">
+                    <img src="" alt="">
+                    <span>Nome Produto</span>
+                    <a href="">comprar</a>
+                </div>
+                <div class="product">
+                    <img src="" alt="">
+                    <span>Nome Produto</span>
+                    <a href="">comprar</a>
+                </div>
+                <div class="product">
+                    <img src="" alt="">
+                    <span>Nome Produto</span>
+                    <a href="">comprar</a>
+                </div>
+                <div class="product">
+                    <img src="" alt="">
+                    <span>Nome Produto</span>
+                    <a href="">comprar</a>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</body>
+</html>
